948c515553a9c0cb01178e7922bf05b4603de019
Chris <jikuusaber@gmail.com>
2014-03-14 13:15:43 -0400

    Dots expand and contract when clicked. GUISkin as an asset.

---
+++ Assets/CheckClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckClick : MonoBehaviour {
+
+	//private Animator expand;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("GameObject initialized");
+		//expand = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown () {
+		animation ["Expand"].wrapMode = WrapMode.PingPong;
+		animation.Play ("Expand");
+		Debug.Log ("MouseClick detected");
+	}
+}
